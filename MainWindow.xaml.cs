c9d0068954cdb94f23d86cd7824f938dd774dc62
ibrahima-github <ibrahimasall.dev@gmail.com>
2021-07-09 01:59:07 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppDataBindingObjet.Model;
+
+namespace WpfAppDataBindingObjet
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Adherent adherent = new Adherent();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //DataContext initialisation, Model: adherent
+            this.DataContext = adherent;
+
+            //Textboxes initialization
+            adherent.Numero = 100; adherent.Nom = "Nom de l'adh 100";
+            
+        }
+
+        //Click event method
+        private void GenererAdherent_Click(object sender, RoutedEventArgs e)
+        {
+            Random r = new Random();
+            int i = r.Next(1, 1001);
+            adherent.Numero = i;
+            adherent.Nom = $"Nom de l'adhérent {i} ";
+
+        }
+    }
+}
---
+++ Model/Adherent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppDataBindingObjet.Model
+{
+  public   class Adherent   :INotifyPropertyChanged 
+    {
+       
+        // Un attribut change à l'appel du setter ,
+        // A ce moment la classe doit Notifier l'iHM 
+        //qui est en liason avec elle
+
+        // profpull + tab + tab
+        private int numero;
+
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value;
+                // A chaque appel du setter il y modification de la proprité
+                //En ce moment on doit Notifier l'IHM ()
+                JaiChange("Numero"); // Annoncer le changement et dire 
+                                       //la propriété qui chanage
+            }
+        }
+         
+        // 
+        private void JaiChange(string vPropriete)
+        {
+           if (PropertyChanged != null) // S'il y a des souscripteurs
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(vPropriete));
+            }
+
+             //    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(vPropriete));
+        }
+
+        private string nom;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value;
+                JaiChange("Nom");
+            }
+        }
+
+
+
+
+    }
+}
